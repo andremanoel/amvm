02008338e196fe2b4332a5ec560df23f553cd24d
andresouza <andrezaum20@gmail.com>
2018-02-01 11:50:25 -0300

    Incluindo mapas do Arcgis

--- application/controllers/ContatoController.php
+++ application/controllers/ContatoController.php
@@ -1,14 +1,58 @@
 <?php
 
-class ContatoController extends Zend_Controller_Action
+class ContatoController extends Core_Controller_Base
 {
 
-    public function init(){
+    public function init()
+    {
         parent::init();
     }
-    
+
     public function indexAction()
-    {}
+    {
+        if ($this->_request->isPost()) {
+            try {
+                $this->validate();
+                $params = $this->getAllParams();
+                $modelContato = new Application_Model_Contato();
+                $objContato   = $modelContato->createRow($this->getAllParams());
+                $objContato->save();
+                $this->addMensagem('Sua mensagem foi enviada. Aguarde a resposta.', 'success');
+                $this->redirect('/contato');
+            } catch (Exception $e) {
+                $this->addMensagem($e->getMessage(), 'warning');
+            }
+        }
+    }
+
+    public function validate()
+    {
+        $params = $this->getAllParams();
+        $nome = $this->limparTexto($params['nome']);
+        if (empty($params['nome'])) {
+            throw new Core_Controller_Exception_Validate('O nome é obrigatório.');    
+        }
+        
+        $email = $this->limparTexto($params['email']);
+        if (empty($params['email'])) {
+            throw new Core_Controller_Exception_Validate('O email é obrigatório.');
+        }
+        
+        $assunto = $this->limparTexto($params['assunto']);
+        if (empty($params['assunto'])) {
+            throw new Core_Controller_Exception_Validate('O assunto é obrigatório.');
+        }
+        
+        $mensagem = $this->limparTexto($params['mensagem']);
+        if (empty($params['mensagem'])) {
+            throw new Core_Controller_Exception_Validate('A mensagem é obrigatória.');
+        }
+    }
+    
+    public function limparTexto($var)
+    {
+        return strip_tags(trim($var));
+    }
 }
 
 
---
+++ application/models/Contato.php
@@ -0,0 +1,8 @@
+<?php
+
+class Application_Model_Contato extends Zend_Db_Table_Abstract
+{
+    protected $_name    = 'tb_contato';
+    protected  $_primary = 'id_contato';
+}
+
